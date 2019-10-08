c92202cbac6a68e51bba1e17605395a04dd9b9eb
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-10-07 21:16:25 -0300

    Atualiza programação

--- semd/settings.py
+++ semd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f5v^px0h4ee1hx+gt=08kvh0*-tede6dd_!&l1ek)+%@!yy_@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['semd.ufpi.br', 'localhost']
 
