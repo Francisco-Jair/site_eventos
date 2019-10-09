0367f6c511ccd67a17722004293cda4d19272aa5
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-10-09 09:09:53 -0300

    Atualiza senha banco de dados

--- semd/settings.py
+++ semd/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'semd',
         'USER': 'semd',
-        'PASSWORD': 'semd',
+        'PASSWORD': '5mG!4Is#',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
