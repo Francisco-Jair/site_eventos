36bb9012eb818eebb0f7f1934d18e2caca82f69d
Gabriel Costa Campos <gabriel.costa.campos.13@gmail.com>
2021-09-10 00:25:14 -0300

    Atualiza static e media root'

--- semd/settings.py
+++ semd/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/var/www/html/semd/static/'
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = '/var/www/html/semd/media/'
+MEDIA_ROOT = '/media/'
 
 django_heroku.settings(locals())
