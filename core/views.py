e6672db46b83a5982a84dd4eb855143593a0f56f
Gabriel Costa Campos <gabriel.costa.campos.13@gmail.com>
2021-09-16 06:58:21 -0300

    Adiciona envio de email

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.views.decorators.csrf import csrf_protect
 from .models import Palestrantes, Minicursos, Usuarios
+from django.core.mail import send_mail
+from django.conf import settings
+import threading
 
 def index(request):
     palestrantes = Palestrantes.objects.all()
@@ -35,6 +38,8 @@ def inscricao(request):
             )
 
             usuario.save()
+
+            enviar_email_registro(usuario)
             
             dados = {'palestrantes': palestrantes, 'minicursos': minicursos, 'mensagem': 'Inscrição cadastrada com sucesso.'}
 
@@ -49,3 +54,22 @@ def inscricao(request):
 
         return render(request, 'index.html', dados)
 
+def email(assunto, mensagem, remetente, destinatarios):
+    send_mail(assunto, mensagem, remetente, destinatarios)
+
+def enviar_email(assunto='', mensagem='', remetente=settings.EMAIL_HOST_USER, destinatarios=[]):
+    thread = threading.Thread(target=email, args=(assunto, mensagem, remetente, destinatarios))
+    thread.start()
+
+def enviar_email_registro(usuario):
+    assunto = 'Inscrição confirmada!'
+    mensagem = f'Olá, {usuario.nome},\n\nParabéns, sua inscrição no II Simpósio Nordestino de Mídias Digitais para a Educação – SiNeMIDE foi confirmada com sucesso.\n\nLembrando que o SiNeMIDE ocorrerá nos dias nos dias 23 e 24 de setembro de 2021.\n\nPara obter mais informações e a nossa programação completa, acesse nosso site: https://bit.ly/2VI72QS.'
+    destinatarios = [usuario.email]
+
+    enviar_email(assunto=assunto, mensagem=mensagem, destinatarios=destinatarios)
+
+def handler404(request, exception):
+    response = render(request, '404.html', {})
+    response.status_code = 404
+    
+    return response
--- semd/settings.py
+++ semd/settings.py
@@ -134,4 +134,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+# Envio de e-mails
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_USE_TLS = True
+
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = 'sinemide@ufpi.edu.br'
+
+EMAIL_HOST_PASSWORD = 'Sinemide.21'
+
 django_heroku.settings(locals())
